f5eb29cd36bec85dc84e79f65154a10edb02c877
KingOfExcuses <kingofexcusestv@gmail.com>
2023-09-02 13:30:38 -0500

    console log current time and slice block hour id

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+	// TODO: Add code to display the current date in the header of the page.
+	var currentDate = dayjs().format('dddd, MMMM D, YYYY')
+	$('#currentDay').text(currentDate)
+	// TODO: Add code to apply the past, present, or future class to each time
+	// block by comparing the id to the current hour. HINTS: How can the id
+	// attribute of each time-block be used to conditionally add or remove the
+	// past, present, and future classes? How can Day.js be used to get the
+	// current hour in 24-hour time?
+	//
+	var currentTime = dayjs().format('h:m')
+	console.log(currentTime)
+	var blockHour = $('.time-block').attr('id').slice(5, 6)
+	console.log(blockHour)
+
+
+
 	// TODO: Add a listener for click events on the save button. This code should
 	// use the id in the containing time-block as a key to save the user input in
 	// local storage. HINT: What does `this` reference in the click listener
@@ -9,17 +25,8 @@ $(function () {
 	// time-block containing the button that was clicked? How might the id be
 	// useful when saving the description in local storage?
 	//
-	// TODO: Add code to apply the past, present, or future class to each time
-	// block by comparing the id to the current hour. HINTS: How can the id
-	// attribute of each time-block be used to conditionally add or remove the
-	// past, present, and future classes? How can Day.js be used to get the
-	// current hour in 24-hour time?
-	//
 	// TODO: Add code to get any user input that was saved in localStorage and set
 	// the values of the corresponding textarea elements. HINT: How can the id
 	// attribute of each time-block be used to do this?
 	//
-	// TODO: Add code to display the current date in the header of the page.
-	var currentDate = dayjs().format('dddd, MMMM D, YYYY')
-	$('#currentDay').text(currentDate)
 })
